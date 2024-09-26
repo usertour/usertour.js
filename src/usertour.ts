26b81c10e5a663b6b70a3b90dce103e2af7817fd
winterissnowing <liuzhaodong@gmail.com>
2024-09-26 14:02:53 +0800

    feat:  added some stubbed implementions of usertour.* methods

--- src/usertour.ts
+++ src/usertour.ts
@@ -13,28 +13,153 @@ export interface Usertour {
 
   load: () => Promise<void>
 
-  init: (envId: string, options?: InitOptions) => void
+  init: (token: string) => void
 
-  identify: (userId: string, attributes?: Attributes) => void
+  identify: (
+    userId: string,
+    attributes?: Attributes,
+    opts?: IdentifyOptions
+  ) => Promise<void>
+
+  identifyAnonymous: (
+    attributes?: Attributes,
+    opts?: IdentifyOptions
+  ) => Promise<void>
+
+  updateUser: (attributes: Attributes, opts?: IdentifyOptions) => Promise<void>
+
+  group: (
+    groupId: string,
+    attributes?: Attributes,
+    opts?: GroupOptions
+  ) => Promise<void>
+
+  updateGroup: (attributes: Attributes, opts?: GroupOptions) => Promise<void>
+
+  track(
+    name: string,
+    attributes?: EventAttributes,
+    opts?: TrackOptions
+  ): Promise<void>
+
+  isIdentified: () => boolean
+
+  start: (contentId: string, opts?: StartOptions) => Promise<void>
+
+  endAll: () => Promise<void>
 
   reset: () => void
 
-  isIdentified: () => boolean
-}
+  remount: () => void
 
-export type InitOptions = {
-  baseZIndex?: string
-  userInfo?: InitUserInfo
+  // eslint-disable-next-line es5/no-rest-parameters
+  on(eventName: string, listener: (...args: any[]) => void): void
+
+  // eslint-disable-next-line es5/no-rest-parameters
+  off(eventName: string, listener: (...args: any[]) => void): void
+
+  setCustomInputSelector(customInputSelector: string | null): void
+
+  registerCustomInput(
+    cssSelector: string,
+    getValue?: (el: Element) => string
+  ): void
+
+  setCustomNavigate(customNavigate: ((url: string) => void) | null): void
+
+  setUrlFilter(urlFilter: ((url: string) => string) | null): void
+
+  setLinkUrlDecorator(linkUrlDecorator: ((url: string) => string) | null): void
+
+  setInferenceAttributeNames(attributeNames: string[]): void
+
+  setInferenceAttributeFilter(
+    attributeName: string,
+    filters: StringFilters
+  ): void
+
+  setInferenceClassNameFilter(filters: StringFilters): void
+
+  setScrollPadding(scrollPadding: ScrollPadding | null): void
+
+  setCustomScrollIntoView(scrollIntoView: ((el: Element) => void) | null): void
+
+  _setTargetEnv(targetEnv: unknown): void
+
+  setShadowDomEnabled(shadowDomEnabled: boolean): void
+
+  setPageTrackingDisabled(pageTrackingDisabled: boolean): void
+
+  setBaseZIndex(baseZIndex: number): void
+
+  setServerEndpoint(serverEndpoint: string | null | undefined): void
+
+  disableEvalJs(): void
 }
 
 export interface Attributes {
-  [name: string]: string | number | boolean | null | undefined
+  [name: string]: AttributeLiteralOrList | AttributeChange
 }
 
-export type InitUserInfo = {
-  id: string
-  name?: string
+type AttributeLiteral = string | number | boolean | null | undefined
+type AttributeLiteralOrList = AttributeLiteral | AttributeLiteral[]
+
+interface AttributeChange {
+  set?: AttributeLiteralOrList
+  set_once?: AttributeLiteralOrList
+  add?: string | number
+  subtract?: string | number
+  append?: AttributeLiteralOrList
+  prepend?: AttributeLiteralOrList
+  remove?: AttributeLiteralOrList
+  data_type?: AttributeDataType
 }
+
+type AttributeDataType = 'string' | 'boolean' | 'number' | 'datetime' | 'list'
+
+export type IdentifyOptions = {
+  signature?: string
+}
+
+export interface GroupOptions {
+  signature?: string
+  membership?: Attributes
+}
+
+export interface EventAttributes {
+  [name: string]: AttributeLiteral | EventAttributeChange
+}
+
+interface EventAttributeChange {
+  set?: AttributeLiteral
+  data_type?: AttributeDataType
+}
+
+export interface TrackOptions {
+  userOnly?: boolean
+}
+
+export interface StartOptions {
+  once?: boolean
+}
+
+export interface ResourceCenterState {
+  isOpen: boolean
+  hasChecklist: boolean
+  uncompletedChecklistTaskCount: number
+  unreadAnnouncementCount: number
+}
+
+interface ScrollPadding {
+  top?: number
+  right?: number
+  bottom?: number
+  left?: number
+}
+
+type StringFilter = ((className: string) => boolean) | RegExp
+
+type StringFilters = StringFilter | StringFilter[]
 
 interface Deferred {
   resolve: () => void
@@ -109,23 +234,22 @@ if (!usertour) {
   }
 
   // Helper to stub promise-returning methods that should be queued
-  // var stubPromise = function (
-  //   // eslint-disable-next-line es5/no-rest-parameters
-  //   method: ConditionalKeys<Usertour, (...args: any[]) => Promise<void>>
-  // ) {
-
-  //   // @ts-ignore
-  //   usertour![method] = function () {
-  //     var args = Array.prototype.slice.call(arguments)
-  //     usertour!.load()
-  //     var deferred: Deferred
-  //     var promise = new Promise<void>(function (resolve, reject) {
-  //       deferred = {resolve: resolve, reject: reject}
-  //     })
-  //     q.push([method, deferred!, args])
-  //     return promise
-  //   } as any
-  // }
+  var stubPromise = function (
+    // eslint-disable-next-line es5/no-rest-parameters
+    method: ConditionalKeys<Usertour, (...args: any[]) => Promise<void>>
+  ) {
+    // @ts-ignore
+    usertour![method] = function () {
+      var args = Array.prototype.slice.call(arguments)
+      usertour!.load()
+      var deferred: Deferred
+      var promise = new Promise<void>(function (resolve, reject) {
+        deferred = {resolve: resolve, reject: reject}
+      })
+      q.push([method, deferred!, args])
+      return promise
+    } as any
+  }
 
   // Helper to stub methods that MUST return a value synchronously, and
   // therefore must support using a default callback until Usertour.js is
@@ -140,10 +264,37 @@ if (!usertour) {
     }
   }
 
+  // Methods that return void and should be queued
   stubVoid('init')
-  stubVoid('identify')
+  stubVoid('off')
+  stubVoid('on')
   stubVoid('reset')
+  stubVoid('setBaseZIndex')
+  stubVoid('setCustomInputSelector')
+  stubVoid('setCustomNavigate')
+  stubVoid('setCustomScrollIntoView')
+  stubVoid('setInferenceAttributeFilter')
+  stubVoid('setInferenceAttributeNames')
+  stubVoid('setInferenceClassNameFilter')
+  stubVoid('setScrollPadding')
+  stubVoid('setServerEndpoint')
+  stubVoid('setShadowDomEnabled')
+  stubVoid('setPageTrackingDisabled')
+  stubVoid('setUrlFilter')
+  stubVoid('setLinkUrlDecorator')
 
+  // Methods that return promises and should be queued
+  stubPromise('endAll')
+  stubPromise('group')
+  stubPromise('identify')
+  stubPromise('identifyAnonymous')
+  stubPromise('start')
+  stubPromise('track')
+  stubPromise('updateGroup')
+  stubPromise('updateUser')
+
+  // Methods that synchronously return and can be stubbed with default return
+  // values and are not queued
   stubDefault('isIdentified', false)
 }
 
